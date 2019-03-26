38ddbf7189dbffd15f13cc68243b24837a73e45e
Alex Rose <rosea@tf.uni-freiburg.de>
2019-03-26 12:30:33 +0100

    shell script tweaks

--- msub.sh
+++ msub.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -x
 
-msub -l nodes=1:ppn=12 sinedensity.sh -o sinedensity
-msub -l nodes=1:ppn=16 shearwave.sh -o shearwave
-msub -l nodes=1:ppn=16 poiseuille.sh -o poiseuille
-msub -l nodes=1:ppn=16 -l walltime=1200 couette.sh -o couette
+mkdir output
+# clean up previous runs
+rm output/*.txt
 
-msub -l nodes=1:ppn=1 -l walltime=1200 timer.sh -o timer
-msub -l nodes=1:ppn=2 -l walltime=900 timer.sh -o timer
-msub -l nodes=1:ppn=4 timer.sh -o timer
-msub -l nodes=1:ppn=8 timer.sh -o timer
-msub -l nodes=1:ppn=16 timer.sh -o timer
-msub -l nodes=2:ppn=16 timer.sh -o timer
-msub -l nodes=2:ppn=28 timer.sh -o timer56.out
-msub -l nodes=4:ppn=28 timer.sh -o timer
+msub -l nodes=1:ppn=12 sinedensity.sh -o output/sinedensity.txt
+msub -l nodes=1:ppn=16 shearwave.sh -o output/shearwave.txt
+msub -l nodes=1:ppn=16 poiseuille.sh -o output/poiseuille.txt
+msub -l nodes=1:ppn=16 -l walltime=1200 couette.sh -o output/couette.txt
+
+msub -l nodes=1:ppn=1 -l walltime=1200 timer.sh -o output/timer1.txt
+msub -l nodes=1:ppn=2 -l walltime=900 timer.sh -o output/timer2.txt
+msub -l nodes=1:ppn=4 timer.sh -o output/timer4.txt
+msub -l nodes=1:ppn=8 timer.sh -o output/timer.txt
+msub -l nodes=1:ppn=16 timer.sh -o output/timer16.txt
+msub -l nodes=2:ppn=16 timer.sh -o output/timer32.txt
+msub -l nodes=2:ppn=28 timer.sh -o output/timer56.txt
+msub -l nodes=4:ppn=28 timer.sh -o output/timer112.txt
--- timer.sh
+++ timer.sh
@@ -3,4 +3,9 @@
 module load devel/python/3.5.2
 module load mpi/openmpi/3.0-gnu-7.1
 
-mpiexec --bind-to core --map-by core python timer.py
+# run for each of three methods we're looking at
+for i in 0 1 2
+do
+echo METHOD i
+mpiexec --bind-to core --map-by core python timer.py $i
+done
